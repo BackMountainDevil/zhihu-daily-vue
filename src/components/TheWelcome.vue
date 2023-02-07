7105aeb05ad33d358a70d942100d5aead59c54ff
mifen <191615342@qq.com>
2023-02-07 09:21:48 +0800

    style: 改动布局css

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -29,6 +29,7 @@ export default {
       axios.get("http://127.0.0.1:8010/news/" + id)
         .then((response) => {
           this.story = response.data
+          scrollTo(0,0) // 页面滚动到顶部
         })
     }
   }
@@ -36,25 +37,61 @@ export default {
 </script>
 
 <template>
-  <div v-if=stories>
-    <h1>每日推荐</h1>
-    <div v-for="item in stories">
-      <div v-if=item.image> <img :src=item.image /></div>
-      <a @click="getContent(item.id)"> {{ item.title }} - {{ item.hint }}</a>
-    </div>
-  </div>
-
-  <div v-if=top_stories>
-    <h1>主题日报</h1>
-    <div v-for="item in top_stories">
-      <div v-if=item.image> <img :src=item.image /></div>
-      <a @click="getContent(item.id)"> {{ item.title }} - {{ item.hint }}</a>
-    </div>
-  </div>
-
-  <div v-if=story>
+  <div class="content" v-if=story>
     <h1>{{ story.title }}</h1>
     <div v-html=story.body></div>
     <a :href=story.share_url>查看原文</a>
   </div>
+
+  <div class="content"  v-if=stories >
+    <h1>每日推荐</h1>
+    <div v-for="item in stories">
+      <div class="left"><img class="image" v-if=item.image :src=item.image /></div>
+      <div class="right"><a class="title" @click="getContent(item.id)"> {{ item.title }} - {{ item.hint }}</a></div>
+    </div>
+  </div>
+
+  <div  class="content" v-if=top_stories>
+    <h1>主题日报</h1>
+    <div>
+      <div v-for="item in top_stories">
+        <div class="left"><img class="image" v-if=item.image :src=item.image /></div>
+      <div class="right"><a class="title" @click="getContent(item.id)"> {{ item.title }} - {{ item.hint }}</a></div>
+      </div>
+    </div>
+  </div>
+
+
 </template>
+
+<style>
+.image {
+  width: 50px;
+  height: 50px;
+}
+
+.title {
+  color: black;
+  font-weight: normal;
+
+  border-radius: 5px;
+}
+
+.left {
+  float: left;
+  width: 60px;
+  height: 50px;
+}
+
+.right {
+  height: 60px;
+  width: 100%;
+  margin-left: 60px;
+  margin-top: 5px;
+}
+
+.content{
+  margin-left: 5%;
+  margin-right: 5%;
+}
+</style>
